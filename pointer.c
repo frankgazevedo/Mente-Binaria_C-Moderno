befaf3caf040b14e8ffc8eda26818081557eab52
FrankAzevedo <frankgazevedo@gmail.com>
2021-10-30 22:49:11 -0400

    Estudo sobre Ponteiros

--- pointer.c
+++ pointer.c
@@ -5,30 +5,52 @@ int main(void){
   int a[] = {2017, 2018, 2019};
 
   for (int i = 0; i < 3; i++)
-    printf("%d\n", a[i]); 
+    printf("a[%d] = %d\n", i, a[i]); 
 
-  printf("O endereço de a[] em memória é: %p\n", a);
+  printf("\nO endereço de a[] em memória é: %p\n", a);
 
+  // Modo 1: método usual
+  printf("\nModo 1: método \"usual\"\n");
   for (int i = 0; i < 3; i++)
-    printf("O conteúdo de a[] é: %d\n", *a + i); // Aritmética de Ponteiros
+    printf("O conteúdo de a[%d] é: %d\n", i, a[i]);
 
+  // Modo 2: métod que utiliza Aritmética de Ponteiros
+  printf("\nModo 2: método que utiliza Aritmética de Ponteiros\n");
+  for (int i = 0; i < 3; i++)
+    printf("O conteúdo de a[%d] é: %d\n", i,  *a + i); // Aritmética de Ponteiros
+
+  printf("\nDeclarando uma variável: int i; \n");
   int i = 2017;
 
-  // declarando um Ponteiro
+  printf("\nDeclarando um Ponteiro: int *p;\n");
   int *p;
 
-  // inicializando um Ponteiro
+  printf("\nInicializando um Ponteiro: p = &i;\n");
   p = &i;
 
-  printf("Endereço de i em memória SEM ponteiro: %p\n", &i);
+  printf("\n");
 
-  printf("Endereço de i em memória COM ponteiro: %p\n", p);
+  printf("Endereço de i em memória SEM ponteiro [&i]: %p\n", &i);
 
-  printf("Conteúdo de i SEM ponteiro: %d\n", i);
+  printf("Endereço de i em memória COM ponteiro [%%p]: %p\n", p);
 
-  printf("Conteúdo de i COM ponteiro: %d\n", *p);
+  printf("Conteúdo de i SEM ponteiro [i]: %d\n", i);
 
+  printf("Conteúdo de i COM ponteiro [*i]: %d\n", *p);
 
+  printf("\nDemonstrando formas diferentes de \"alocar\" Strings\n");
+
+  char s[] = "Frank";
+
+  char *t = "Frank";
+
+  char u[] = { 0x46, 0x72, 0x61, 0x6e, 0x6b, 0x00 };
+
+  puts(s);
+
+  puts(t);
+
+  puts(u);
 
   return 0;
 }
